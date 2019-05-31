56dd4e55c307a05e3fbcfd8a6a882e1a61a6c011
Trusilov Oleksandr <oleksandr.trusilov@gmail.com>
2019-05-31 15:56:14 +0300

    fix frontend

--- counter/views.py
+++ counter/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 import operator
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/accounts/signup')
 def home(request):
     return render(request, 'counter/home.html')
 
